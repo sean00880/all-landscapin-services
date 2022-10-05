1154521c80554a127795d691f4852c75c74cce44
sean00880 <theelitereapersanctum@gmail.com>
2022-10-04 23:03:32 -0400

    new3

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ export const data = [
     name: "mowing",
     title: "Mowing",
     desc: "Your lawn will be trimmed in addition to being mowed. Within the turf area, our personnel will handle all line trimming around obstacles and structures and use a power edger to edge any pathways, sidewalks, and hard edges++899.",
-    photo: "https://i.ibb.co/ykytH0n/mowing.png",
+    photo: "mowing.png",
     longDesc:
       "Your lawn will be trimmed in addition to being mowed. Within the turf area, our personnel will handle all line trimming around obstacles and structures and use a power edger to edge any pathways, sidewalks, and hard edges. The debris left over after pruning and mowing will subsequently be blown off of all the property's hard surfaces.",
     images: [

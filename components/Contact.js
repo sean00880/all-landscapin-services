5f272c3d9bd796071da777b80f3f2c9c2c67e156
sean00880 <theelitereapersanctum@gmail.com>
2022-10-12 03:21:44 -0400

    update5

--- components/Contact.js
+++ components/Contact.js
@@ -13,11 +13,19 @@ const Contact = () => {
         <h1 className={style.subtitle}>Get Your Free Estimate<hr></hr>All Landscaping Services, LLC</h1>
         </Flip>
       <form className={style.form}>
+      <div>
+          <h2>3 steps</h2>
+          1 - Review our list of services provided. <br />
+          2 - Reach out to us for a free estimate. <br />
+          3 - Schedule a visit for your exciting landscaping project.
+        </div>
+        <hr />
         <div>
         <input className={style.inputS} type="text" placeholder="Name" />
         <input className={style.inputS} type="text" placeholder="Phone" />
         <input className={style.inputL} type="text" placeholder="Email" />
         <input className={style.inputL} type="text" placeholder="Subject" />
+        <hr />
         <textarea
           className={style.textArea}
           type="text"
@@ -26,13 +34,7 @@ const Contact = () => {
         />
         <button className={style.button}>SUBMIT</button>
         </div>
-        <hr />
-        <div>
-          <h2>3 steps</h2>
-          1 - Review our list of services provided. <br />
-          2 - Reach out to us for a free estimate. <br />
-          3 - Schedule a visit for your exciting landscaping project.
-        </div>
+
       </form>
       <div >
         <Flip right>

78274e80fb0c003e24b8cb0ec31b42cd1ac0dd44
sean00880 <theelitereapersanctum@gmail.com>
2022-10-14 00:00:47 -0400

    new

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      
+      <Head>
+      <meta name="google-site-verification" content="dc24KWyRESpQWDA8kk0H2jHprXUGDALBCxfVM7834KU" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
